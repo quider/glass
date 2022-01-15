f2f655ad16acfd5b3770ec3f2f61c1ab1ef8b8cf
Adrian <adrian.kozlowski@outlook.com>
2022-01-15 15:03:30 +0100

    initial commit

---
+++ glass-backend/src/test/java/pl/adriankozlowski/glass/glassbackend/GlassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adriankozlowski.glass.glassbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlassBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
