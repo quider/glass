223faa4388e98272b1ea6d52c27a8f257a4077c9
Adrian <adrian.kozlowski@outlook.com>
2022-01-15 16:24:42 +0100

    some objects

---
+++ glass-backend/src/main/java/pl/adriankozlowski/glass/glassbackend/model/Device.java
@@ -0,0 +1,4 @@
+package pl.adriankozlowski.glass.glassbackend.model;
+
+public abstract class Device {
+}
---
+++ glass-backend/src/main/java/pl/adriankozlowski/glass/glassbackend/model/GlassHouse.java
@@ -0,0 +1,12 @@
+package pl.adriankozlowski.glass.glassbackend.model;
+
+import java.util.List;
+
+public class GlassHouse {
+    private List<Device> devices;
+
+    public GlassHouse(List<Device> devices) {
+        this.devices = devices;
+    }
+
+}
